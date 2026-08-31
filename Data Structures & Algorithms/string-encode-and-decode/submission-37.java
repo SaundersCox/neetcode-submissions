1b35f2327f09be8c2eb48e8341024e5c33ebd9d6
Saunders <34501844+SaundersCox@users.noreply.github.com>
2026-08-31 16:27:36 -0400

    Add: string-encode-and-decode - submission-37

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-37.java
@@ -0,0 +1,28 @@
+class Solution {
+
+    public String encode(List<String> strs) {
+        // encode as <num><delim><str>
+        StringBuilder sb = new StringBuilder();
+        for (String str : strs) {
+            sb.append(str.length()).append('@').append(str);
+        }
+        return sb.toString();
+    }
+
+    public List<String> decode(String str) {
+        List<String> result = new ArrayList<>();
+        int l = 0;
+        int r = 0;
+        while (l < str.length()) {
+            while (str.charAt(r) != '@') {
+                r++;
+            }
+            int len = Integer.parseInt(str.substring(l, r));
+            l = r + 1;
+            r = l + len;
+            result.add(str.substring(l, r));
+            l = r;
+        }
+        return result;
+    }
+}
