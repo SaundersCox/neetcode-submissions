23df2f8e28eb75a0e3f6391eb403565013c2b540
Saunders <34501844+SaundersCox@users.noreply.github.com>
2026-09-12 14:07:30 -0400

    Add: valid-sudoku - submission-57

---
+++ Data Structures & Algorithms/valid-sudoku/submission-57.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        // valid != solvable
+        // rows, cols, boxes, all valid sets of 9
+        // skip all '.'
+        for (int i = 0; i < 9; i++) {
+            Set<Character> row = new HashSet<>();
+            Set<Character> col = new HashSet<>();
+            Set<Character> box = new HashSet<>();
+            for (int j = 0; j < 9; j++) {
+                char c = board[i][j];
+                if (c != '.' && !row.add(c)) {
+                    return false;
+                }
+                c = board[j][i];
+                if (c != '.' && !col.add(c)) {
+                    return false;
+                }
+                c = board[j/3 + i/3*3][j%3 + i%3*3];
+                if (c != '.' && !box.add(c)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
